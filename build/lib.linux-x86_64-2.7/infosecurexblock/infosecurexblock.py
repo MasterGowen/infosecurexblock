e171caa942dbf006970297117327343c2695095f
root <mastergowen@gmail.com>
2018-05-03 14:10:56 +0500

    first

---
+++ build/lib.linux-x86_64-2.7/infosecurexblock/infosecurexblock.py
@@ -0,0 +1,95 @@
+"""TO-DO: Write a description of what this XBlock is."""
+
+import pkg_resources
+import datetime
+import json
+
+import django.utils.translation
+
+from xblock.core import XBlock
+from xblock.fields import Scope, Integer, String
+from xblock.fragment import Fragment
+
+from webob.response import Response
+
+import subprocess
+
+
+from .utils import (
+    load_resource,
+    render_template,
+    load_resources,
+)
+
+class InfoSecureXBlock(XBlock):
+    """
+    TO-DO: document what your XBlock does.
+    """
+
+    # Fields are defined on the class.  You can access them in your code as
+    # self.<fieldname>.
+
+    # TO-DO: delete count, and define your own fields.
+    count = Integer(
+        default=0, scope=Scope.user_state,
+        help="A simple counter, to show something happening",
+    )
+    display_name = String(default="infosecurexblock", scope=Scope.settings)
+
+    def resource_string(self, path):
+        """Handy helper for getting resources from our kit."""
+        data = pkg_resources.resource_string(__name__, path)
+        return data.decode("utf8")
+
+    # TO-DO: change this view to display your data your own way.
+    def student_view(self, context=None):
+        """
+        The primary view of the InfoSecureXBlock, shown to students
+        when viewing courses.
+        """
+        context = {
+        "display_name": self.display_name,
+        }
+        fragment = Fragment()
+        fragment.add_content(
+            render_template(
+                "static/html/infosecurexblock.html",
+                context
+            )
+        )
+        js_urls = ("static/js/src/infosecurexblock.js",)
+        css_urls = ("static/css/infosecurexblock.css",)
+        load_resources(js_urls, css_urls, fragment)
+        fragment.initialize_js('InfoSecureXBlock')
+        return fragment
+
+    # TO-DO: change this handler to perform your own actions.  You may need more
+    # than one handler, or you may not need any handlers at all.
+    @XBlock.json_handler
+    def increment_count(self, data, suffix=''):
+        """
+        An example handler, which increments the data.
+        """
+        # Just to show data coming in...
+        assert data['hello'] == 'world'
+
+        self.count += 1
+        return {"count": self.count}
+
+    # TO-DO: change this to create the scenarios you'd like to see in the
+    # workbench while developing your XBlock.
+    @staticmethod
+    def workbench_scenarios():
+        """A canned scenario for display in the workbench."""
+        return [
+            ("InfoSecureXBlock",
+             """<infosecurexblock/>
+             """),
+            ("Multiple InfoSecureXBlock",
+             """<vertical_demo>
+                <infosecurexblock/>
+                <infosecurexblock/>
+                <infosecurexblock/>
+                </vertical_demo>
+             """),
+        ]
---
+++ infosecurexblock/__init__.py
@@ -0,0 +1,1 @@
+from .infosecurexblock import InfoSecureXBlock
