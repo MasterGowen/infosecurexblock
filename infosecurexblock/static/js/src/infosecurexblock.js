f402a0fb3bc0cfbf7020c48d91e487093c6cce60
MasterGowen <mastergowen@gmail.com>
2018-05-03 15:38:10 +0500

    test#13

--- infosecurexblock/static/js/src/infosecurexblock.js
+++ infosecurexblock/static/js/src/infosecurexblock.js
@@ -10,7 +10,7 @@ function InfoSecureXBlock(runtime, element) {
         $.ajax({
             type: "POST",
             url: rect1HandlerUrl,
-            data: "ggg",
+            data: JSON.stringify({"ggg": "ggg"}),
             success: success_check
         });
 
