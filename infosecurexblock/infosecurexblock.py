48e1fd387c2aa3bc12b80ef7303e491a0b92e2dd
anonimD08 <asdebug@yandex.ru>
2018-05-06 11:27:34 +0500

    #add_images_11_try2

--- infosecurexblock/infosecurexblock.py
+++ infosecurexblock/infosecurexblock.py
@@ -72,7 +72,7 @@ class InfoSecureXBlock(XBlock):
         #      file_icon=self.runtime.local_resource_url(self, "static/images/file.svg"),
         #      wifi_icon=self.runtime.local_resource_url(self, "static/images/wifi.svg"),
         # )
-        css_urls = ("static/css/infosecurexblock.css",#css_context)
+        css_urls = ("static/css/infosecurexblock.css",)#css_context
         load_resources(js_urls, css_urls, fragment)
         fragment.initialize_js('InfoSecureXBlock')
         return fragment
