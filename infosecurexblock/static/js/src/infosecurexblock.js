549a6bf16c3267bde7ebc3b050d8d7e9fcfb9668
Anastasiya <ovchinnickova.anast@gmail.com>
2018-05-30 18:59:56 +0500

    lab3

--- infosecurexblock/static/js/src/infosecurexblock.js
+++ infosecurexblock/static/js/src/infosecurexblock.js
@@ -308,7 +308,9 @@ function InfoSecureXBlock(runtime, element) {
             //console.log("ON funcction:",this);
             var evt = event.target;
             var student_answer = {};
+            
             if (evt.id =="Link1"){
+                console.log(evt.id);
                 document.getElementById('lab3_links').value = document.getElementById("link1").value; 
             }
             if (evt.id == "checkid2"){
