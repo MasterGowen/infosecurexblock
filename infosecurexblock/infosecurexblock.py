222bdcc78dac043f4a40ed19067a3afb7402f271
Ovchinnickova2016 <ovchinnickova.anast@gmail.com>
2018-06-02 19:23:39 +0500

    lab3check18

--- infosecurexblock/infosecurexblock.py
+++ infosecurexblock/infosecurexblock.py
@@ -170,7 +170,7 @@ class InfoSecureXBlock(StudioEditableXBlockMixin, XBlock):
                 test = int(data["link"])
                 if((test == 1) or (test == 5) or (test == 11) or (test==4) or (test ==8) or (test==9) or (test>=13)):
                     self.right=0.1+self.right
-                return sum(float(self.right))
+                return self.right
 
 
         def IsTheNumberSimple(n):
