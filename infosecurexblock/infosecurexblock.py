a5edf2660671d956a07c90c9c9f33834ff87da77
Anastasiya <ovchinnickova.anast@gmail.com>
2018-06-02 13:48:47 +0500

    lab3_check5

--- infosecurexblock/infosecurexblock.py
+++ infosecurexblock/infosecurexblock.py
@@ -169,7 +169,7 @@ class InfoSecureXBlock(StudioEditableXBlockMixin, XBlock):
                 test = int(data["link"])
                 right = 0.0
                 if((test == 1) or (test == 5) or (test == 11) or (test==4) or (test ==8) or (test==9) or (test>=13)):
-                    right=0.1 + right
+                    right=0.1
                     return sum(float(right))
                 else:
                     return 0
