d7201b7ce66a0890011e81696c7d5e09da18a6be
Anastasiya <ovchinnickova.anast@gmail.com>
2018-06-03 12:02:13 +0500

    lab3_check

--- infosecurexblock/static/js/src/infosecurexblock.js
+++ infosecurexblock/static/js/src/infosecurexblock.js
@@ -292,7 +292,7 @@ function InfoSecureXBlock(runtime, element) {
             evt.id == "Link1" && linkFixate([".link1","Link1"]) && l1;
             evt.id == "Link2" && linkFixate([".link2","Link2"])&& l2;
             evt.id == "Link3" && linkFixate([".link3","Link3"]) &&l3;
-            evt.id == "Link4" && linkFixate([".link4","Link4"]);
+            evt.id == "Link4" && linkFixate([".link4","Link4"])&&l4;
             evt.id == "Link5" && linkFixate([".link5","Link5"]);
             evt.id == "Link6" && linkFixate([".link6","Link6"]);
             evt.id == "Link7" && linkFixate([".link7","Link7"]);
@@ -317,7 +317,7 @@ function InfoSecureXBlock(runtime, element) {
                     'link1' : document.querySelector(idNum[0]).getAttribute(l1),
                     'link2' : document.querySelector(idNum[0]).getAttribute(l2),
                     'link3' : document.querySelector(idNum[0]).getAttribute("id"),
-                    'link4' : document.querySelector(idNum[0]).getAttribute("id"),
+                    'link4' : document.querySelector(idNum[0]).getAttribute(l4),
                     'link5' : document.querySelector(idNum[0]).getAttribute("id"),
                     'link6' : document.querySelector(idNum[0]).getAttribute("id"),
                     'link7' : document.querySelector(idNum[0]).getAttribute("id"),
