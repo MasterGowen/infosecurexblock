025e01ae952c8c05f5606133c2720517acfd3174
anonimD08 <asdebug@yandex.ru>
2018-06-03 13:37:57 +0500

    #Labs#1

--- infosecurexblock/static/js/src/infosecurexblock.js
+++ infosecurexblock/static/js/src/infosecurexblock.js
@@ -158,7 +158,7 @@ function InfoSecureXBlock(runtime, element) {
         static connectionLabs3(handler, labId) {
             var self = this;
             //console.log(lab_id);
-            console.log(result.lab_id);
+            //console.log(result.lab_id);
             function success(handler) {
                 if (handler.Rect1) {
                     handler.amount = Object.keys(handler.Rect1).length;
