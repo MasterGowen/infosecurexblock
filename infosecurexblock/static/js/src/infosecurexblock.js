93b63da0de7482f53504ca560f069b3e463664ef
anonimD08 <asdebug@yandex.ru>
2018-06-05 20:13:44 +0500

    #test1

--- infosecurexblock/static/js/src/infosecurexblock.js
+++ infosecurexblock/static/js/src/infosecurexblock.js
@@ -79,11 +79,11 @@ function InfoSecureXBlock(runtime, element) {
             case 4:{
                // Start.connectionLabs(rect1HandlerUrl, 1);
                // Start.connectionLabs(rect1HandlerUrl, 2);
+                document.getElementById('taskTextID').innerHTML = $('.task_text', element).text();
+                document.getElementById('widget').addEventListener('click',Start.taskBlock);
                 document.getElementById('widget').addEventListener('click', Start.onLab4);
                 document.getElementById("widget").addEventListener('mouseover',Start.onLab4styleActive);
                 document.getElementById("widget").addEventListener('mouseout',Start.onLab4styleDeactive);
-                document.getElementById('taskTextID').innerHTML = $('.task_text', element).text();
-                document.getElementById('widget').addEventListener('click',Start.taskBlock);
                 break;
             }
            // console.log('succesCheckLAB : ',result.lab_id);
