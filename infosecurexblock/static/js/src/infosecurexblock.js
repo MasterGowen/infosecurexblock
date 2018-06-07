ec71a3c669925dafa9e3e90f9cee8212f57a8a35
Anastasiya <ovchinnickova.anast@gmail.com>
2018-06-07 17:57:33 +0500

    lab1_random5

--- infosecurexblock/static/js/src/infosecurexblock.js
+++ infosecurexblock/static/js/src/infosecurexblock.js
@@ -221,7 +221,7 @@ function InfoSecureXBlock(runtime, element) {
                     document.getElementById(jsonObj[amount].idnum).innerHTML += jsonObj[amount].value;
                     }
                 }
-            function fileShow(){
+            function fileShow(amount){
                 Start.active(['File1Id','File1TextID','File1TextID2']);
                 addElementTextSVG2(amount, jsonObj);
             }
