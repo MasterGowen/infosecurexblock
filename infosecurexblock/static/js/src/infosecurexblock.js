fe5b74d9820dda123fbec1f472e55adb28cd1e95
Anastasiya <ovchinnickova.anast@gmail.com>
2018-06-08 11:58:26 +0500

    lab1

--- infosecurexblock/static/js/src/infosecurexblock.js
+++ infosecurexblock/static/js/src/infosecurexblock.js
@@ -232,7 +232,8 @@ function InfoSecureXBlock(runtime, element) {
             function fileShow(){
                 console.log("keys :" ,keys, mes); 
                 Start.active(['File1Id','File1TextID','File1TextID2',keys, mes]);
-             var global_N = Start.value2([keys]);
+             var global_N_1 = Start.value2([keys]);
+                var st = global_N_1.indexOf("Ключ:");
                 console.log("id --- ",global_N);
                 if (id =="key_id1" )
               {  var global_d= "2";
