e22984774664bea76a9f9e5e78499568ae0f4c98
anonimD08 <asdebug@yandex.ru>
2018-06-08 15:27:29 +0500

    #studio_test1

--- infosecurexblock/static/js/src/infosecurexblock_studio.js
+++ infosecurexblock/static/js/src/infosecurexblock_studio.js
@@ -1,5 +1,49 @@
 function InfoSecureXBlock(runtime, element) {
 
+    document.querySelector("#scenario-settings-tab").onclick = function () {
+        document.querySelector("#main-settings-tab").classList.remove("is-active-tabs");
+        document.querySelector("#scenario-settings-tab").classList.add("is-active-tabs");
+        document.querySelector("#advanced-settings-tab").classList.remove("is-active-tabs");
+        document.querySelector("#main-settings").setAttribute("hidden", "true");
+        document.querySelector("#scenario-settings").removeAttribute("hidden");
+        document.querySelector("#advanced-settings").setAttribute("hidden", "true");
+      };
+    
+      document.querySelector("#advanced-settings-tab").onclick = function () {
+        document.querySelector("#main-settings-tab").classList.remove("is-active-tabs");
+        document.querySelector("#scenario-settings-tab").classList.remove("is-active-tabs");
+        document.querySelector("#advanced-settings-tab").classList.add("is-active-tabs");
+        document.querySelector("#main-settings").setAttribute("hidden", "true");
+        document.querySelector("#scenario-settings").setAttribute("hidden", "true");
+        document.querySelector("#advanced-settings").removeAttribute("hidden");
+      };
+
+      $(element).find(".save-button").bind("click", function() {
+
+        var handlerUrl = runtime.handlerUrl(element, "studio_submit"),
+            data = {
+                "display_name": $(element).find("input[name=display_name]").val(),
+                "task_text": $(element).find("textarea[name=question]").val(),
+                "weight": $(element).find("input[name=weight]").val(),
+                "max_attempts": $(element).find("input[name=max_attempts]").val(),
+                "lab_id": $(element).find("select[name=lab_id]").val(),
+            };
+
+        $.post(handlerUrl, JSON.stringify(data)).done(function (response) {
+
+            window.location.reload(true);
+
+        });
+
+    });
+
+
+    $(element).find(".cancel-button").bind("click", function () {
+
+        runtime.notify("cancel", {});
+
+    });
+
     class Start {
         constructor() {
             this.defaultSet = {
