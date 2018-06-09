5947bc50f3faf0a973dad2ddb2ef88ceffea36e1
MasterGowen <mastergowen@gmail.com>
2018-06-09 16:35:00 +0500

    mixin16

--- infosecurexblock/static/js/src/infosecurexblock_studio.js
+++ infosecurexblock/static/js/src/infosecurexblock_studio.js
@@ -4,13 +4,26 @@ function StudioEditableXBlockMixin(runtime, element) {
 
     //monkeypatching
 
-    var tabButtons = '<ul class="editor-modes action-list action-modes">' +
-        '<li class="is-action-item" data-mode="editor">' +
-        '<a href="#" class="editor-button is-set">Редактор</a>' +
-        '</li><li class="is-action-item" data-mode="settings">' +
-        '<a href="#" class="settings-button">Настройки</a></li></ul>'
+    var tabList = '<li class="action-tabs is-active-tabs" id="settings-tab">Основные</li><li class="action-tabs" id="advanced-settings-tab">Расширенные</li>';
+    document.getElementsByClassName("editor-modes action-list action-modes")[0].innerHTML = tabList;
 
-    $('.modal-header').append(tabButtons);
+    document.querySelector('#settings-tab').onclick = function(){
+	  document.querySelector('#settings-tab').classList.add('is-active-tabs');
+	  document.querySelector('#advanced-settings-tab').classList.remove('is-active-tabs');
+
+	  document.querySelector('#main-settings').removeAttribute('hidden');
+      document.querySelector('#advanced-settings').setAttribute('hidden', 'true');
+	};
+
+	document.querySelector('#advanced-settings-tab').onclick = function(){
+	  document.querySelector('#settings-tab').classList.remove('is-active-tabs');
+	  document.querySelector('#advanced-settings-tab').classList.add('is-active-tabs');
+
+	  document.querySelector('#main-settings').setAttribute('hidden', 'true');
+      document.querySelector('#advanced-settings').removeAttribute('hidden');
+};
+
+
 
     var fields = [];
     var tinyMceAvailable = (typeof $.fn.tinymce !== 'undefined'); // Studio includes a copy of tinyMCE and its jQuery plugin
