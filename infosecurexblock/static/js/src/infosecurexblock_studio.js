1ccd5692326cead5245435cf2376c52a56136cba
MasterGowen <mastergowen@gmail.com>
2018-06-09 15:00:09 +0500

    mixin8

--- infosecurexblock/static/js/src/infosecurexblock_studio.js
+++ infosecurexblock/static/js/src/infosecurexblock_studio.js
@@ -169,4 +169,51 @@ function StudioEditableXBlockMixin(runtime, element) {
         e.preventDefault();
         runtime.notify('cancel', {});
     });
+    class Start {
+        constructor() {
+            this.defaultSet = {
+                x: 0,
+                y: 0,
+                width: 850,
+                height: 850,
+                fill: '#f5f5f5',
+                class: 'rect2'
+            }
+            this.star = () => {
+                //new Labs();
+                this.createElementSVG('svg');
+                this.appendNodeSVG(this.constract('rect'));
+            }
+        }
+
+        constract(name) {
+            return this.createElementSVG(name, this.defaultSet);
+        }
+
+        createElementSVG(name, attributes) {
+            this.NS = "http://www.w3.org/2000/svg";
+            this.element = document.createElementNS(this.NS, name);
+            console.log(this.element);
+            if (name == "svg") {
+                this.element.setAttributeNS(null, 'id', 'star_studio');
+                document.getElementById("widget_studio").appendChild(this.element);
+            }
+            if (attributes) {
+                for (var k in attributes) {
+                    this.element.setAttributeNS(null, [k], attributes[k]);
+                }
+            }
+            return this.element;
+        }
+        appendNodeSVG(element) {
+            var svg = document.getElementById('star_studio');
+            return svg.appendChild(element);
+        }
+    }
+
+    (function () {
+        /* Here's where you'd do things on page load. */
+        var test = new Start();
+        test.star();
+})();
 }
