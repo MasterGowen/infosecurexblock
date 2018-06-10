8174ef7eca2e57863431940c2127512773fc9ec1
Anastasiya <ovchinnickova.anast@gmail.com>
2018-06-10 20:30:28 +0500

    lab5

--- infosecurexblock/infosecurexblock.py
+++ infosecurexblock/infosecurexblock.py
@@ -237,10 +237,7 @@ class InfoSecureXBlock(StudioEditableXBlockMixin, XBlock):
                 answer0 = data["e"]
                 key = data["key"]
                 if (key=="key_id1"):
-                    if ((answer0=="ИНФОРМАЦИОННАЯ БЕЗОПАСНОСТЬ") or (answer0=="информационная безопасность") or (answer0=="Информационная безопасность")):
-                        return 1
-                    else:
-                        return 0
+                    return 1
                 if (key=="key_id2"):
                     if ((answer0=="ИНФОРМАТИКА") or (answer0=="Информатика") or (answer0=="информатика")):
                         return 1
