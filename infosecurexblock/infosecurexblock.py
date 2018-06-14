9819c8f47e54d532d17f5fd9266fbf4f16de53dd
Anastasiya <ovchinnickova.anast@gmail.com>
2018-06-14 08:03:03 +0500

    lab3

--- infosecurexblock/infosecurexblock.py
+++ infosecurexblock/infosecurexblock.py
@@ -234,7 +234,7 @@ class InfoSecureXBlock(StudioEditableXBlockMixin, XBlock):
                                     data["link15"],
                                     data["link16"],
                                     ]
-                return sum(correctness_list) / float(len(correctness_list) - 6)
+                return sum(correctness_list) / 16)
 
             elif self.lab_id == 4:
                 pass
