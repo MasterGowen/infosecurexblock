87527da3a4686e9fd58e9d4a7767ded6d8742748
Anastasiya <ovchinnickova.anast@gmail.com>
2018-06-16 16:01:36 +0500

    lab1

--- infosecurexblock/infosecurexblock.py
+++ infosecurexblock/infosecurexblock.py
@@ -211,9 +211,8 @@ class InfoSecureXBlock(StudioEditableXBlockMixin, XBlock):
                     right = [22, 10, 26, 10, 15, 4]
                 elif (key == "mes_id10"):
                     right = [5, 16, 19, 20, 21, 17, 1]
-                if IsTheNumberSimple(d):
-                    for j, k in enumerate(copy.deepcopy(right)):
-                        right[j] = right[j] ** d % N
+                for j, k in enumerate(copy.deepcopy(right)):
+                    right[j] = right[j] ** d % N
                 if (str(right) == str(answer2)) & (ip == "192.168.0.4"):
                     return 1
                 else:
